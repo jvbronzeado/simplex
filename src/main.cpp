cd8b38c8537bd90ac1a93f08beec1b9f602422fc
jvbronzeado <jv.bronzeado@hotmail.com>
2026-06-06 16:47:37 -0300

    fixed meson build, added eigen, added mps reader

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
+#include "mpsReader.h"
+
 int main(const int argc, const char* argv[]) {
     std::cout << "hello cpp" << std::endl;
+
+    mpsReader reader = mpsReader();
     return 0;
 }
